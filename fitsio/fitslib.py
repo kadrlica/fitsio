1569a44b21d9e8c48a34900c450407b244acd2d7
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-07-24 01:17:48 -0500

    Allow 'CONTINUE' keyword with equals sign (#130)

--- fitsio/fitslib.py
+++ fitsio/fitslib.py
@@ -4306,7 +4306,8 @@ class FITSCard(FITSRecord):
             self._check_len()
 
             front=card_string[0:7]
-            if (not self.has_equals() or front=='COMMENT' or front=='HISTORY'):
+            if (not self.has_equals() or front=='COMMENT' or front=='HISTORY'
+                or front=='CONTINU'):
 
                 #if front=='CONTINU':
                 #    raise ValueError("CONTINUE not supported")
